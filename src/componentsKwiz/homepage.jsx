cdabd57b56210d5d342b73e49a40012aa5d318a1
Matija Zuber <matijazuber273@gmail.com>
2026-03-26 08:50:07 +0100

    added the bacgkround image, removed the vite default styles, created a homepage for the user to start the game

---
+++ src/componentsKwiz/homepage.jsx
@@ -0,0 +1,16 @@
+const Homepage = () => {
+  return (
+    <>
+      <main>
+        <div className="name">Kwiz</div>
+        <p className="homepage-paragraph">
+          Think you know it all? Put your knowledge to the test across
+          categories like science, history, pop culture, and more.{" "}
+        </p>
+        <button className="homepage-button">Start kwiz</button>
+      </main>
+    </>
+  );
+};
+
+export default Homepage;
